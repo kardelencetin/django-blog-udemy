3e40b98c64d4e819fbe37f25c6f6bd29ff102249
kardelencetin <krdlnctn98@gmail.com>
2022-05-24 14:52:36 +0300

    yazi admin paneli düzenlendi

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
-from blog.models import KategoriModel
+from blog.models import KategoriModel, YazilarModel
+
+
+class YazilarAdmin(admin.ModelAdmin):
+    list_display = ('title', 'created', 'moderated')
+    search_fields = ('title', 'content')
 
 admin.site.register(KategoriModel)
-
+admin.site.register(YazilarModel, YazilarAdmin)
--- blog/models/yazi.py
+++ blog/models/yazi.py
@@ -1,14 +1,14 @@
-from enum import unique
-from statistics import mode
-from tabnanny import verbose
 from autoslug import AutoSlugField
 from django.db import models
 from blog.models import KategoriModel
 from django.contrib.auth.models import User
+from ckeditor.fields import RichTextField
+
+
 class YazilarModel(models.Model):
     resim = models.ImageField(upload_to='yazi_resimleri')
     title = models.CharField(max_length=50)
-    content = models.TextField()
+    content = RichTextField()
     created = models.DateTimeField(auto_now_add=True)
     moderated = models.DateTimeField(auto_now=True)
     yazi_slug = AutoSlugField(populate_from='title', unique=True)
@@ -18,4 +18,7 @@ class YazilarModel(models.Model):
     class Meta:
         verbose_name = 'Yazi'
         verbose_name_plural = 'Yazilar'
-        db_name = 'Yazi'
+        db_table = 'Yazi'
+
+    def __str__(self):
+        return self.title
